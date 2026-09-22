d559f8c332637ba902245dd804ed7aed0b86c591
Nguyễn Duy Hưng <nguyenduyhung3624@gmail.com>
2026-09-22 08:27:54 +0700

    fix: proxy MangaDex cover URLs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,7 @@ const fallbackCover = 'https://placehold.co/120x170/1c1c1c/ffffff?text=MANGA'
 const getCoverUrl = (manga: Manga, size: 256 | 512 = 256) => {
   const cover = manga.relationships.find((item) => item.type === 'cover_art')
   return cover?.attributes?.fileName
-    ? `https://uploads.mangadex.org/covers/${manga.id}/${cover.attributes.fileName}.${size}.jpg`
+    ? `/api/mangadex/covers/${manga.id}/${cover.attributes.fileName}.${size}.jpg`
     : fallbackCover
 }
 
